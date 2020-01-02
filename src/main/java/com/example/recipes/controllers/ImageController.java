04cf2539c3fabc447cd1fbea8dd92b7c77be7167
Majid Qureshi <mpq1990@gmail.com>
2020-01-02 12:34:21 +0100

    feat: add image upload form - in progress

--- src/main/java/com/example/recipes/controllers/ImageController.java
+++ src/main/java/com/example/recipes/controllers/ImageController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ImageController {
@@ -21,5 +24,11 @@ public class ImageController {
     public String showUploadForm(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/imageuploadform";
+    }
+
+    @PostMapping("recipe/{id}/image")
+    public String handleImagePost(@PathVariable String id, @RequestParam("imagefile") MultipartFile file) {
+        imageService.saveImageFile(Long.valueOf(id), file);
+        return "redirect:/recipe/" + id + "/show";
     }
 }
--- src/test/java/com/example/recipes/controllers/ImageControllerTest.java
+++ src/test/java/com/example/recipes/controllers/ImageControllerTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class ImageControllerTest {
@@ -45,5 +47,21 @@ class ImageControllerTest {
                 andExpect(status().isOk()).
                 andExpect(view().name("recipe/imageuploadform")).
                 andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    void handleImagePost() throws Exception {
+        MockMultipartFile mockMultipartFile= new MockMultipartFile(
+                "imagefile",
+                "testing.txt",
+                "text/plain",
+                "some text here".getBytes());
+
+        mockMvc.perform(multipart("/recipe/1/image").file(mockMultipartFile)).
+                andExpect(status().is3xxRedirection()).
+                andExpect(header().string("Location", "/recipe/1/show"));
+
+        verify(imageService, times(1)).saveImageFile(anyLong(), any());
+
     }
 }
